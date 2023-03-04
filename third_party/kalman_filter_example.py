5bdd3ba6586cd48f691ac9e2539d9b8f4eec0648
Youssef OUMATE <ysf.ing.ysf@gmail.com>
2023-03-04 15:40:38 +0900

    start changing example to bbox

--- third_party/kalman_filter_example.py
+++ third_party/kalman_filter_example.py
@@ -26,20 +26,22 @@ if __name__ == "__main__":
     img_height = 500
     img_width = 500
     kalman = cv2.KalmanFilter(2, 1, 0)
+    bbox_w = 10
+    bbox_h = 20
 
     code = long(-1)
 
     cv2.namedWindow("Kalman")
 
     while True:
-        state = 0.1 * np.random.randn(2, 1)
+        state = 0.1 * np.random.randn(4, 1)
 
-        kalman.transitionMatrix = np.array([[1., 1.], [0., 1.]])
-        kalman.measurementMatrix = 1. * np.ones((1, 2))
-        kalman.processNoiseCov = 1e-5 * np.eye(2)
+        kalman.transitionMatrix = np.array([[1., 1., 1., 1.], [0., 1., 1., 1.], [0. ,0. ,1., 1.], [0., 0., 0., 1.]])
+        kalman.measurementMatrix = 1. * np.ones((1, 4))
+        kalman.processNoiseCov = 1e-5 * np.eye(4)
         kalman.measurementNoiseCov = 1e-1 * np.ones((1, 1))
-        kalman.errorCovPost = 1. * np.ones((2, 2))
-        kalman.statePost = 0.1 * np.random.randn(2, 1)
+        kalman.errorCovPost = 1. * np.ones((4, 4))
+        kalman.statePost = 0.1 * np.random.randn(4, 1)
 
         while True:
             def calc_point(angle):
@@ -48,6 +50,7 @@ if __name__ == "__main__":
 
             state_angle = state[0, 0]
             state_pt = calc_point(state_angle)
+            print(state_pt)
 
             prediction = kalman.predict()
             predict_angle = prediction[0, 0]
@@ -62,6 +65,13 @@ if __name__ == "__main__":
             measurement_pt = calc_point(measurement_angle)
 
             # plot points
+            def draw_bbox(center, color, d):
+                top = center[1] - bbox_w
+                left = center[0] - bbox_h
+                bottom = center[1] + bbox_w
+                right = center[0] + bbox_h
+                cv2.rectangle(img, (top, left), (bottom, right), color, 1, cv2.LINE_AA, 0)
+
             def draw_cross(center, color, d):
                 cv2.line(img,
                          (center[0] - d, center[1] - d), (center[0] + d, center[1] + d),
@@ -72,15 +82,16 @@ if __name__ == "__main__":
 
             img = np.zeros((img_height, img_width, 3), np.uint8)
             draw_cross(np.int32(state_pt), (255, 255, 255), 3)
-            draw_cross(np.int32(measurement_pt), (0, 0, 255), 3)
-            draw_cross(np.int32(predict_pt), (0, 255, 0), 3)
+            draw_bbox(np.int32(state_pt), (0, 255, 0), 3)
+            #draw_cross(np.int32(measurement_pt), (0, 0, 255), 3)
+            #draw_cross(np.int32(predict_pt), (0, 255, 0), 3)
 
-            cv2.line(img, state_pt, measurement_pt, (0, 0, 255), 3, cv2.LINE_AA, 0)
-            cv2.line(img, state_pt, predict_pt, (0, 255, 255), 3, cv2.LINE_AA, 0)
+            #cv2.line(img, state_pt, measurement_pt, (0, 0, 255), 3, cv2.LINE_AA, 0)
+            #cv2.line(img, state_pt, predict_pt, (0, 255, 255), 3, cv2.LINE_AA, 0)
 
             kalman.correct(measurement)
 
-            process_noise = sqrt(kalman.processNoiseCov[0,0]) * np.random.randn(2, 1)
+            process_noise = sqrt(kalman.processNoiseCov[0,0]) * np.random.randn(4, 1)
             state = np.dot(kalman.transitionMatrix, state) + process_noise
 
             cv2.imshow("Kalman", img)
