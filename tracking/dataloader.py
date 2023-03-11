b5a6c9d8243bf9a2f1317599859363fd495016aa
Youssef OUMATE <ysf.ing.ysf@gmail.com>
2023-03-11 10:24:58 +0900

    use a kalman filter for each point

--- tracking/dataloader.py
+++ tracking/dataloader.py
@@ -10,35 +10,30 @@ class Sampling():
 
     def __init__(self):
         #num. of candidates
+        self.img_size = 512
         self.num_samples = 50
         #initial box
         self.gt = np.array([10., 10., 40., 80.])
+        self.filters = []
+        self.init_state = self.norm_sampling(self.gt)
         #kalman params
-        self.kalman = cv2.KalmanFilter(self.gt.shape[0],
-                                       self.num_samples * self.gt.shape[0], 0)
-        self.kalman.transitionMatrix = np.array([
-            [1., 0., 1., 0.], [0., 1., 0., 1.], [0., 0., 1., 0.],
-            [0., 0., 0., 1.]
-        ]).astype(np.float32)  # np.dot(kalman.transitionMatrix, state)
-        self.kalman.measurementMatrix = 1. * np.eye(self.num_samples * self.gt.shape[0]).astype(
-                np.float32)  #np.dot(kalman.measurementMatrix, state)
-        self.kalman.processNoiseCov = 1e-5 * np.eye(self.gt.shape[0]).astype(np.float32) #sqrt(kalman.processNoiseCov[0,0]) * np.random.randn(50, 1)
-        dt = 1
-        q = 1e-5
-        self.kalman.processNoiseCov = q * np.array(
-            [[dt**3 / 3, dt**2 / 2, dt**2 / 2, dt], [dt**2 / 2, dt, dt, q],
-             [dt**2 / 2, dt, dt**3 / 3, dt**2 / 2], [dt, q, dt**2 / 2, q]]).astype(
-                np.float32
-            )
-        
-        self.kalman.measurementNoiseCov = 1e-1 * np.eye(
-            self.num_samples * self.gt.shape[0]).astype(
-                np.float32
-            )  #kalman.measurementNoiseCov * np.random.randn(1, 1)
-        self.kalman.statePre = np.array([self.gt[0], self.gt[1], 0, 0]).astype(np.float32)
-        self.kalman.errorCovPre = np.eye(self.gt.shape[0]).astype(np.float32)
+        for i in range(self.num_samples):
+            kalman = cv2.KalmanFilter(4, 2) # State size: 4 (x, y, dx, dy), Measurement size: 2 (x, y)
+            kalman.transitionMatrix = np.array([[1., 0., 1., 0.], # State transition model
+                                            [0., 1., 0., 1.],
+                                            [0., 0., 1., 0.],
+                                            [0., 0., 0., 1.]]).astype(np.float32)
+            kalman.measurementMatrix = np.array([[1., 0., 0., 0.], # Measurement model
+                                            [0., 1., 0., 0.]]).astype(np.float32)
+            kalman.processNoiseCov = np.eye(4).astype(np.float32) * (10 ** -3) # Process noise covariance
+            kalman.measurementNoiseCov = np.eye(2).astype(np.float32) * (10 ** -2) # Measurement noise covariance
+            kalman.errorCovPost = np.eye(4).astype(np.float32) * (10 ** -3) # Posteriori error estimate covariance matrix
+            kalman.statePost = np.array([self.init_state[i][0], self.init_state[i][1], 
+                                    np.random.randn(), 
+                                    np.random.randn()], dtype=np.float32).reshape(-1,1) # Initial state estimate
+            self.filters.append(kalman)
 
-    def norm_sampling(self, img_size, bb, n):
+    def norm_sampling(self, bb):
         # bb: target bbox (min_x,min_y,w,h)
         bb = np.array(bb, dtype='float32')
         trans = 1
@@ -46,30 +41,30 @@ class Sampling():
         # (center_x, center_y, w, h)
         sample = np.array([bb[0] + bb[2] / 2, bb[1] + bb[3] / 2, bb[2], bb[3]],
                           dtype='float32')
-        samples = np.tile(sample[None, :], (n, 1))
+        samples = np.tile(sample[None, :], (self.num_samples, 1))
         samples[:, :2] += trans * np.mean(bb[2:]) * np.clip(
-            0.5 * np.random.randn(n, 2), -1, 1)
-        samples[:, 2:] *= scale**np.clip(0.5 * np.random.randn(n, 1), -1, 1)
-        samples[:, 2:] = np.clip(samples[:, 2:], 10, img_size - 10)
+            0.5 * np.random.randn(self.num_samples, 2), -1, 1)
+        samples[:, 2:] *= scale**np.clip(0.5 * np.random.randn(self.num_samples, 1), -1, 1)
+        samples[:, 2:] = np.clip(samples[:, 2:], 10, self.img_size - 10)
         samples[:, :2] = np.clip(samples[:, :2], samples[:, 2:] / 2,
-                                 img_size - samples[:, 2:] / 2 - 1)
+                                 self.img_size - samples[:, 2:] / 2 - 1)
         samples[:, :2] -= samples[:, 2:] / 2
         return samples
 
-    def kalman_sampling(self, img_size, bb, n):
+    def kalman_sampling(self, bb):
         # Initialize state with ground truth
-        state = self.norm_sampling(img_size, bb, n)
-
-        # Predict new state
-        prediction = self.kalman.predict()
-        print(prediction)
-        # Generate measurement noise
-        print(self.kalman.measurementMatrix)
-        measurement = state.reshape(-1,1)
-        print(measurement.shape)
-        # Correct the prediction based on the measurement
-        self.kalman.correct(measurement)
-        print(prediction[0][0])
+        state = self.norm_sampling(bb)
+        measurements = []
+        for state_idx in range(len(state)):
+            measurements.append([state[state_idx][0], state[state_idx][1]])
+        measurements = np.array(measurements).astype(np.float32)
+        for sample_idx in range(self.num_samples):
+            prediction = self.filters[sample_idx].predict()[:2] # Predict next state of point i
+            correction = self.filters[sample_idx].correct(measurements[sample_idx].reshape(-1,1))[:2] # Correct state estimate with measurement of point i
+            prediction_x,prediction_y= prediction.flatten().astype(int)
+            correction_x ,correction_y= correction.flatten().astype(int)
+            measured_x ,measured_y= measurements[sample_idx].astype(int)
+            print(prediction)
         exit()
         return prediction
 
@@ -77,7 +72,7 @@ class Sampling():
         for offset in range(25):
             self.gt[:2] = self.gt[:2] + offset
             gt = self.gt
-            samples = self.kalman_sampling(512, gt, self.num_samples)
+            samples = self.kalman_sampling(gt)
             black_img = np.zeros((512, 512, 3), dtype=np.uint8)
             cv2.rectangle(black_img, (gt[0], gt[1]),
                           (gt[0] + gt[2], gt[1] + gt[3]), (0, 255, 0), 1)
