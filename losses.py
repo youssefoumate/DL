fe6cce13044ae276853d2d5b450e9c9323624616
Youssef OUMATE <ysf.ing.ysf@gmail.com>
2023-03-18 09:55:53 +0900

    loss

---
+++ losses.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+bce_loss = nn.BCELoss()
+
+if __name__ == "__main__":
+    m = nn.Sigmoid()
+    input = torch.randn(3, requires_grad=True)
+    target = torch.empty(3).random_(2)
+    output = bce_loss(m(input), target)
+    print(output)
