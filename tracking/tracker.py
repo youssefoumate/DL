092302f66286b0caa80ffcd7263d92370223b299
youssefoumate <ysf.ing.ysf@gmail.com>
2023-03-22 16:40:26 +0900

    generate frames from a dummy video

--- tracking/tracker.py
+++ tracking/tracker.py
@@ -1,16 +1,24 @@
+import sys
 from model import ResNet
+sys.path.append("..")
+from utils.dummy_video import create_dummy_video
+import cv2
 class Tracker():
     def __init__(self) -> None:
         self.backbone = ResNet()
+        self.num_frames = 1000
         pass
-    def init_train(self, first_frame=None, epochs=10):
+    def init_train(self, init_frame=None, epochs=10):
         #TODO: finetune the model on the first frame for 10 epochs
         pass
     def track(self):
-        #TODO: inference the model on the rest of the frames
-        pass
+        frame_gen = create_dummy_video(self.num_frames)
+        init_frame = next(frame_gen)
+        for frame in frame_gen:
+            cv2.imshow("frame", frame)
+            cv2.waitKey(10)
+        cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     tracker = Tracker()
-    tracker.init_train()
     tracker.track()
