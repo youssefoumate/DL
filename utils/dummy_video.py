5bcd13b3f8f962ea54cd41f1c37b931fd94b0cbe
Youssef OUMATE <ysf.ing.ysf@gmail.com>
2023-11-14 18:36:48 +0900

    generate images from video

--- utils/dummy_video.py
+++ utils/dummy_video.py
@@ -1,6 +1,32 @@
 import cv2
 import numpy as np
 
+def cvt_video2img(path):
+    # Create a VideoCapture object and read from input file
+    # If the input is the camera, pass 0 instead of the video file name
+    cap = cv2.VideoCapture(path)
+    
+    # Check if camera opened successfully
+    if (cap.isOpened()== False): 
+        print("Error opening video stream or file")
+    
+    # Read until video is completed
+    frame_id = 0
+    while(cap.isOpened()):
+        frame_id += 1
+        if frame_id % 100 == 0:
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+            if ret == True:
+                cv2.imwrite(f"input/{frame_id}.jpg", frame)
+            else: 
+                break
+    # When everything done, release the video capture object
+    cap.release()
+    
+    # Closes all the frames
+    cv2.destroyAllWindows()
+    
 # Create a blank black image
 def create_dummy_video(num_frames = 50):
 
